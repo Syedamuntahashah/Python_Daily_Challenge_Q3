496fc5305c444f32ddeb7ba8671f733904af0761
Syeda Muntaha Shah <161372867+Syedamuntahashah@users.noreply.github.com>
2025-02-26 16:05:58 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+         # TASK 2 Daily Python Challenge
+        # Date 26 feb 2025
+
+
+# Ask for the user's name and print it with the Processing message
+name = input("Enter your name: ")
+print("🔹 Processing... Name detected: " + name)
+
+# Print the welcom message using string formatting
+print(f"🔹 Access Granted! Welcome, Agent {name}. your mission awaits!🚀".format(name))
