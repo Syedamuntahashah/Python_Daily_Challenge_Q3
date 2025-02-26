16ee032e003cd5f55c4b429c92ebd36dbcb17584
Syeda Muntaha Shah <161372867+Syedamuntahashah@users.noreply.github.com>
2025-02-26 16:05:36 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+                # TASK : 1
+            # Date 25 feb 2025
+
+
+# Write a Python program that prints the Greeting message
+
+print("🔹 Hello, World! Welcome to Python Programming. Let's start coding!🚀")
